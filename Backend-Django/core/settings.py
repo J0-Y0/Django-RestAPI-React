3b26bfe7500ac86d5d9ded15f5a21c26b9dab038
yosef <yosfemyau@gmail.com>
2024-04-23 21:30:15 +0300

    setting-up backend and frontend

--- Backend-Django/core/settings.py
+++ Backend-Django/core/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
