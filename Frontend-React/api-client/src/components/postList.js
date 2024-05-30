62e2c8193884c10fdd8afd26bdd47f15f58615dd
yosef <yosfemyayu@gmail.com>
2024-05-30 14:29:47 +0300

    displaying the posts geting from djangi-rest-api

--- Frontend-React/api-client/src/components/postList.js
+++ Frontend-React/api-client/src/components/postList.js
@@ -1,10 +1,12 @@
-import RecipeReviewCard from './post';
+import Post from './post';
 
 export default function PostList({posts}) {
-    return (
-        posts.map(post =>
-        <RecipeReviewCard post={post} key={post.id} />
-        )
-
+    return (<div className='p-5 d-flex flex-column   justify-content-center'>
+            {posts.map(post =>
+                    
+                        <Post post={post} key={post.id} />
+                    
+                )}
+        </div>
 )
 }
